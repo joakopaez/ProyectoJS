37d9dadb3c6cf767e014570548604ea989df6986
Joaquín Páez <joapmar@gmail.com>
2024-12-26 16:14:29 -0300

    Cambio del sistema a un flitrador de juegos, estado compelto, agregando más juegos

--- main.js
+++ main.js
@@ -1,22 +1,177 @@
-let usuario= prompt ("Ingrese su nombre")
-while(usuario == ""){
-    alert("Dato invalido")
-    usuario= prompt ("Ingrese su nombre")
+let Game = function(name , year , genre , development){
+    this.name = name
+    this.year = year
+    this.genre = genre
+    this.development = development
 }
-let prestamo=(prompt("¿Cuánto dinero necesitas "+usuario+"?"))
-let cuotas=(prompt ("¿En cuántas cuotas podés pagarlo?"))
 
+let game1 = new Game ("SPACE INVADERS", 1978, "SHOOT'EM UP","TAITO")
+let game2 = new Game ("PAC-MAN", 1980, "LABERINTO", "NAMCO")
+let game3 = new Game ("DONKEY KONG", 1981, "PLATAFORMAS", "NINTENDO" )
+let game4 = new Game ("GALAGA", 1981, "SHOOT'EM UP", "NAMCO")
+let game5 = new Game ("TETRIS", 1984, "ROMPECABEZAS", "THE TETRIS COMPANY")
+let game6 = new Game ("SUPER MARIO BROS.", 1985, "PLATAFORMAS", "NINTENDO")
+let game7 = new Game ("THE LEGEND OF ZELDA", 1986, "AVENTURA", "NINTENDO")
+let game8 = new Game ("CONTRA", 1987, "RUN AND GUN", "KONAMI")
+let game9 = new Game ("DOUBLE DRAGON", 1987, "BEAT'EM UP", "TECHNOS JAPON")
+let game10 = new Game ("MEGA MAN 2", 1988, "PLATAFORMAS", "CAPCOM")
+let game11 = new Game ("PRINCE OF PERSIA", 1989, "PLATAFORMAS", "BRODERBUND")
+let game12 = new Game ("THE SECRET OF MONKY ISLAND", 1990, "AVENTURA", "LUCASARTS")
+let game13 = new Game ("SUPER MARIO WORLD", 1990, "PLATAFORMAS", "NINTENDO")
+let game14 = new Game ("SONIC THE HEDGEDOG", 1991, "PLATAFORMAS", "SEGA")
+let game15 = new Game ("STREET FIGHTER 2", 1991, "LUCHA", "CAPCOM")
+let game16 = new Game ("SUNSET RIDERS", 1991, "SHOOT'EM UP", "KONAMI")
+let game17 = new Game ("WOLFENSTEIN 3D", 1992, "F.P.S", "APOGEE SOFTWARE")
+let game18 = new Game ("DOOM", 1993, "F.P.S", "ID SOFTWARWE")
+let game19 = new Game ("MORTAL KOMBAT 2", 1993, "LUCHA", "MIDWAY")
+let game20 = new Game ("EARTHBOUND", 1994, "JUEGO DE ROL", "NINTENDO")
+let game21 = new Game ("SUPER METROID", 1994, "AVENTURA", "NINTENDO")
+let game22 = new Game ("CHRONO TRIGGER", 1995, "JUEGO DE ROL", "SQUARE")
+let game23 = new Game ("WIPEOUT", 1995, "CARRERAS", "PSTGNOSIS")
+let game24 = new Game ("RESIDENT EVIL", 1996, "TERROR", "CAPCOM")
+let game25 = new Game ("TOM RAIDER", 1996, "AVENTURAS", "EIDOS INTERACTIVE")
+let game26 = new Game ("CASTELVANIA", 1997, "AVENTURAS", "KONAMI")
+let game27 = new Game ("STAR FOX", 1997, "SHOOT'EM UP", "NINTENDO")
+let game28 = new Game ("TEKKEN 3", 1997, "LUCHA", "NAMCO")
+let game29 = new Game ("GRIM FANDANGO", 1998, "AVENTURA", "LUCASARTS")
+let game30 = new Game ("METAL GEAR SOLID", 1998, "SIGILO", "KONAMI")
+let game31 = new Game ("SOULCALIBUR", 1998, "LUCHA", "NAMCO")
+let game32 = new Game ("OREGEON TRAIL", 1971, "ESTRATEGIA", "MECC")
+let game33 = new Game ("PONG", 1972, "DEPORTES", "ATARI")
+let game34 = new Game ("ZORK", 1977, "AVENTURA", "INFOCOM")
+let game35 = new Game ("ASTEROIDES", 1979, "SHOOT'EM UP", "ATARI")
+let game36 = new Game ("DEFENDER", 1981, "SHOOT'EM UP", "WILLIAMS ENTERTAINMENT")
+let game37 = new Game ("FROGGER", 1981, "ACCIÓN", "SEGA")
+let game38 = new Game ("GALAGA", 1981, "SHOOT'EM UP", "NAMCO")
+let game39 = new Game ("JOUST", 1982, "ACCIÓN", "WILLIAMS ENTERTAINMENT")
+let game40 = new Game ("MS. PAC-MAN", 1982, "LABERINTO", "MIDWAY")
+let game41 = new Game ("PITFALL!", 1982, "PLATAFORMAS", "ACTIVISON")
+let game42 = new Game ("ROBOTRON:2084", 1982, "SHOOTER", "WILLIAMS ENTERTAINMENT")
+let game43 = new Game ("ELITE", 1984, "SIMULACIÓN ESPACIAL", "ACORNSOFT")
+let game44 = new Game ("GAUNTLET", 1984, "ROL", "ATARI")
+let game45 = new Game ("MIKE TYSON'S PUNCH-OUT!!", 1987, "DEPORTES", "NINTENDO")
+let game46 = new Game ("R-TYPE", 1987, "SHOOT'EM UP", "IREM")
+let game47 = new Game ("SIMCITY", 1989, "CONSTRUCCIÓN DE CIUDADES", "MAXIS")
+let game48 = new Game ("THE LEGEND OF ZELDA: A LINK TO THE PAST", 1991, "AVENTURA", "NINTENDO")
+let game49 = new Game ("LEMINGS", 1991, "ROMPECABEZAS", "PSYGNOSIS")
+let game50 = new Game ("MONKEY ISLAND 2: LECHUCK'S REVENGE", 1991, "AVENTURA", "LUCASARTS")
+let game51 = new Game ("SONIC THE HEDGEHOG 2", 1992, "PLATAFORMAS", "SEGA")
+let game52 = new Game ("SUPER MARIO KART", 1992, "CARRERAS", "NINTENDO")
+let game53 = new Game ()
+let game54 = new Game ()
+let game55 = new Game ()
+let game56 = new Game ()
+let game57 = new Game ()
+let game58 = new Game ()
+let game59 = new Game ()
+let game60 = new Game ()
+let game61 = new Game ()
+let game62 = new Game ()
+let game63 = new Game ()
+let game64 = new Game ()
+let game65 = new Game ()
+let game66 = new Game ()
+let game67 = new Game ()
+let game68 = new Game ()
+let game69 = new Game ()
+let game70 = new Game ()
+let game71 = new Game ()
+let game72 = new Game ()
+let game73 = new Game ()
+let game74 = new Game ()
+let game75 = new Game ()
+let game76 = new Game ()
+let game77 = new Game ()
+let game78 = new Game ()
+let game79 = new Game ()
+let game80 = new Game ()
+let game81 = new Game ()
+let game82 = new Game ()
+let game83 = new Game ()
+let game84 = new Game ()
+let game85 = new Game ()
+let game86 = new Game ()
+let game87 = new Game ()
+let game88 = new Game ()
+let game89 = new Game ()
+let game90 = new Game ()
+let game91 = new Game ()
+let game92 = new Game ()
+let game93 = new Game ()
+let game94 = new Game ()
+let game95 = new Game ()
+let game96 = new Game ()
+let game98 = new Game ()
+let game99 = new Game ()
+let game100 = new Game ()
+let game101 = new Game ()
+let game102 = new Game ()
+let game103 = new Game ()
 
-function interes (){
-    if (cuotas <=6) {interes= 1.2}
-    else if(cuotas >6 && cuotas<=18) {interes=1.8}
-    else {interes=2.2}
-}
-function prestamototal (){
-    if(parseInt(prestamo)){
-    let resultado= (prestamo*interes)
-    alert ("Deberá pagar " + resultado+ " en "+cuotas+" cuotas de "+(resultado/cuotas)+ " cada una")
+let library = [game1,game2,game3,game4,game5,game6,game7,game8,game9,game10,game11,game12,game13,game14,game15,game16,game17,game18,game19,game20,game21,game22,game23,game24,game25,game26,game27,game28,game29,game30,game31]
+
+function addgame() {
+    alert ("No pudimos encotrar tu juego, por favor agreagalo")
+    let name = prompt("Nombre del juego")
+    let year = prompt ("¿En qué año se lanzó el juego?")
+    let genre = prompt ("¿Cuál es el genero del juego?")
+    let development = prompt ("¿Qué empresa desarrolló el juego?")   
+    if (name === "" || isNaN(year) || genre === "" || development === ""){
+        alert ("Dato/s inválido/s")
+        return
+        }
+    let newgame = new Game (name,year,genre,development)
+    library.push(newgame)
+    alert("Juego agregado exitosamente, muchas gracias")    
     }
+
+function filterLibrary() {
+    let filteredLibrary = library;
+
+let searchYear = prompt("¿En qué año se lanzó el juego?");
+    if (searchYear) {
+        let year = parseInt(searchYear)
+        if (isNaN(year) || year > 2025 || year < 1978) {
+            alert("Año inválido");
+            return}
+        filteredLibrary = filteredLibrary.filter((x) => x.year === year);
+        if (filteredLibrary.length === 0) {
+            alert("No se encontraron juegos de ese año");
+            addgame();
+            return
+            
+        }
+    }
+
+let searchGenre = prompt("¿De qué género es tu juego?");
+    if (searchGenre) {
+        searchGenre = searchGenre.toUpperCase();
+        filteredLibrary = filteredLibrary.filter((x) => x.genre === searchGenre);
+        if (filteredLibrary.length === 0) {
+            alert("No se encontraron juegos de este género");
+            addgame();
+            return
+        }
+    }
+
+let searchDevelopment = prompt("¿Cuál es la distribuidora del juego?");
+    if (searchDevelopment) {
+        searchDevelopment = searchDevelopment.trim().toUpperCase();
+        filteredLibrary = filteredLibrary.filter((x) => x.development.toUpperCase() === searchDevelopment);
+        if (filteredLibrary.length === 0) {
+            alert("No se encontraron juegos de esa distribuidora");
+            addgame();
+            return
+        }
+    }
+
+if (filteredLibrary.length === 0) {
+    alert("No se encontraron juegos que cumplan con los criterios. Puedes agregarlo.");
+    addgame()} 
+    else {
+let gameNames = filteredLibrary.map((game) => game.name);
+    alert("Tu juego es: " + gameNames);
 }
-interes()
-prestamototal()
+}
+alert("Piensa en un videojuego y trataré de adivinarlo")
+filterLibrary()
